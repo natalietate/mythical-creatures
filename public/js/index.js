ab23027c061b6afcc1fbf8af184961f8096befad
natalietate <martin.nat@gmail.com>
2017-10-25 15:29:31 -0400

    add starting dashes to match selected answer yay

--- public/js/index.js
+++ public/js/index.js
@@ -13,33 +13,41 @@ let hints = ['Lives in water has no feet', 'has large feet', 'one horn', 'breath
 let guesses, triesCounter, prevTries, hint, correctGuesses
 // i = 0
 
+
 // Game Logic //
 function startGame() {
   triesCounter = 5;
   prevTries = [];
   randomAnswer = answer[Math.floor(Math.random() * answer.length)].split('')
-  // need to display something that shows how long the answer is
+  console.log(randomAnswer)
+  state = [].concat(randomAnswer).fill('_')
+  console.log(state)
   correctGuesses = [];
+  playSpace.textContent = correctGuesses;
   // hintArea.textContent = hints[i]
   tries.textContent = triesCounter;
   prevTriesSpan.textContent = '';
   resultDiv.textContent = 'Pick a letter!'
+
 }
 
-// function renderPrevTries() {
-//   prevTriesSpan.textContent = prevTries.join(', ');
-// }
+function answerLines() {
+  console.log(randomAnswer.length)
+  playSpace.textContent = randomAnswer.length
+}
 
 function isValidGuess(guess) {
   return guess && !prevTries.includes(guess);
 }
 
-function changeTriesCounter() {
+function changeLives() {
   triesCounter--;
   tries.textContent = triesCounter;
   prevTriesSpan.textContent = prevTries.join(', ');
-
 }
+
+
+
 
 // Event Listener aka ~THE GAME~
 form.addEventListener('submit', function(event) {
@@ -47,22 +55,21 @@ form.addEventListener('submit', function(event) {
   const guess = form.guess.value
 
   if (isValidGuess(guess)) {
-    console.log(randomAnswer.indexOf(guess))
+    // console.log(randomAnswer.indexOf(guess)) // what we're using to decide if it is right or wrong -1 means wrong
 
     if (randomAnswer.indexOf(guess) >= 0) {
-      // somehow add letter guessed to playSpace display area in corresponding position...
       correctGuesses.push(guess); // add to the correct guesses array
-      playSpace.textContent = correctGuesses
-      console.log(correctGuesses) // show the array
+      playSpace.textContent = correctGuesses.join(' ');
+      console.log('Correct guesses: '+ correctGuesses) // show the array
+      console.log('Index of the correct guess within answer word: ' + randomAnswer.indexOf(guess)) // this is correct, need to use that to add it to the array in the right order
     } else if (randomAnswer.indexOf(guess) === -1 && triesCounter > 1) {
       resultDiv.textContent = 'Try another letter!';
       prevTries.push(guess); //add to list of letters tried
       console.log(prevTries);
-      changeTriesCounter();
-      // renderPrevTries(); // show on page
+      changeLives();
     } else if (randomAnswer.indexOf(guess) === -1 && triesCounter === 1) {
       resultDiv.textContent = 'You lose!';
-      changeTriesCounter();
+      changeLives();
     }
   } else {
     resultDiv.textContent = 'Input must be a letter and you must not have already guessed it!'
