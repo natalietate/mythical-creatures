cf6420f61327a2891d6e7acb6cf2649553fedf7e
natalietate <martin.nat@gmail.com>
2017-10-28 11:22:27 -0400

    comment JS

--- public/js/index.js
+++ public/js/index.js
@@ -13,9 +13,8 @@ const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 const playAgain = document.querySelector('.play-again');
 
-hintArea.addEventListener('click', function() {
-  hintArea.textContent = 'Hint: ' + hint;
-});
+// Event Listeners //
+hintArea.addEventListener('click', function() {hintArea.textContent = 'Hint: ' + hint;});
 restartBtn.addEventListener('click', startGame);
 
 
@@ -27,6 +26,7 @@ let guesses, guessesLeft, prevTries, hint, correctGuesses, hideModal, showModal,
 
 // Game Logic //
 
+//Pick an answer and corresponding hint from the array
 function chooseAnswer() {
   pickNum = Math.floor(Math.random() * answer.length);
   randomAnswer = answer[pickNum].split('');
@@ -34,20 +34,32 @@ function chooseAnswer() {
   state = [...randomAnswer].fill(' _ ');
   displayState = [...randomAnswer].fill(' _ ').join('');
   feedbackDiv.textContent = '';
-  console.log(randomAnswer);
-  console.log(state);
+  // console.log(randomAnswer);
+  // console.log(state);
 }
 
+// Remove chosen answer from the array of answers so player doesn't get repeats
+function removeAnswer() {
+  answer.splice(pickNum, 1);
+  if (answer.length === 0) {
+    alert("WOW! You beat the whole game.");
+    // need to add some kind of congratulations message here - as a modal? With a new game buton
+  }
+}
+
+// Used to ensure guess is one letter that hasn't been tried yet
 function isValidGuess(guess) {
   return guess && guess.length === 1 && guess.match(/[a-z]/i) && !prevTries.includes(guess);
 }
 
+// Updates the guesses counter and adds incorrect letter to list of letters guessed
 changeLives = function() {
   guessesLeft--;
   tries.textContent = guessesLeft;
   prevTriesSpan.textContent = prevTries.join(', ');
 };
 
+// Check if player has won the round, if so display a winner modal
 function checkWin(state) {
   if (state.includes(' _ ')) {
     return false;
@@ -58,18 +70,13 @@ function checkWin(state) {
   }
 }
 
-function removeAnswer() {
-  answer.splice(pickNum, 1);
-  if (answer.length === 0) {
-    alert("WOW! You beat the whole game.");
-    // need to add some kind of congratulations message here - as a modal? With a new game buton
-  }
-}
-hideModal = function() {
-  modal.classList.add('hidden');
-};
+// Winner modal things
 showModal = function() {
   modal.classList.remove('hidden');
+};
+
+hideModal = function() {
+  modal.classList.add('hidden');
 };
 
 hideModalPlay = function() {
@@ -77,6 +84,7 @@ hideModalPlay = function() {
   startGame();
 };
 
+// Used to return to the beginning nad start new game
 function startGame() {
   chooseAnswer();
   removeAnswer();
@@ -92,27 +100,27 @@ function startGame() {
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   const guess = form.guess.value;
-
   if (isValidGuess(guess)) {
-
     if (randomAnswer.indexOf(guess) >= 0) {
-      // correctGuesses.push(guess); // add to the correct guesses array
-      for (let i = 0; i < randomAnswer.length; i++) { // goes through the whole word so duplicate letters work
-        if (randomAnswer[i] === guess) { // i is comparing the index and matching against the guess index
-          state[i] = guess; // if they match, then replace the same state index with the guess
+      for (let i = 0; i < randomAnswer.length; i++) {
+        if (randomAnswer[i] === guess) {
+          state[i] = guess;
         }
       }
-      console.log(state);
-      playSpace.textContent = state.join(' '); // update with the matching letters found
+      // console.log(state);
+      playSpace.textContent = state.join(' ');
       checkWin(state); // was this the last guess needed for a win?
-
     } else if (randomAnswer.indexOf(guess) === -1 && guessesLeft > 1) {
       feedbackDiv.textContent = 'That\'s not quite right. Try a different letter!';
-      prevTries.push(guess); //add to list of letters tried
-      // console.log(prevTries);
+      prevTries.push(guess);
+      // The feedback will only display for a few seconds
+      setTimeout(function() {
+        feedbackDiv.textContent='';
+      }, 3000);
       changeLives();
     } else if (randomAnswer.indexOf(guess) === -1 && guessesLeft === 1) {
       changeLives();
+      // displays feedback and then automatically restarts the game
       feedbackDiv.innerHTML = `<h1>Oh no, you lost!</h1>`;
       setTimeout(function() {
         startGame();
@@ -121,6 +129,7 @@ form.addEventListener('submit', function(event) {
   } else {
     feedbackDiv.textContent = 'Uh oh! Please guess something else. Your guess must be a single letter that you have not previously tried.';
   }
+
   form.guess.value = '';
 });
 
