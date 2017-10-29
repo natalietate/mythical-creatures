c73712fcd8d649a738b90a3bde5cce9cbd74fb2d
natalietate <martin.nat@gmail.com>
2017-10-29 09:56:43 -0400

    final updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,27 @@
 /*jshint esversion: 6 */
-// add a closure!
+
 (function() {
+
   // HTML Elements //
   const form = document.forms[0];
   const tries = document.getElementById('tries');
   const prevTriesSpan = document.getElementById('letters-guessed');
   const feedbackDiv = document.getElementById('feedback');
-  const restartBtn = document.getElementById('restart');
   const hintArea = document.getElementById('hint');
   const playSpace = document.getElementById('play-space');
   const modal = document.querySelector('.modal');
   const close = document.querySelector('.close');
   const playAgain = document.querySelector('.play-again');
 
-  // Event Listeners //
+  // Event Listener //
   hintArea.addEventListener('click', function() {
     hintArea.textContent = 'Hint: ' + hint;
   });
-  restartBtn.addEventListener('click', startGame);
-
 
   // Game Data //
   const answer = ['mermaid', 'bigfoot', 'unicorn', 'dragon', 'centaur', 'wizard', 'werewolf', 'pegasus', 'troll', 'yeti'];
-  const hints = ['She lives under the sea', 'Inhabits forests, especially in the Pacific Northwest', 'If this were a tech company, it would be worth more than $1 billion', 'Found in the sky', 'Half and half', '✨✨✨', 'Check the moon', 'Pure white, like the clouds', 'Also found in the comments section', 'Winter is his favorite season'];
-  let guesses, guessesLeft, prevTries, hint, correctGuesses, hideModal, showModal, pickNum;
-
+  const hints = ['She lives under the sea', 'Inhabits forests, especially in the Pacific Northwest', 'If this were a tech company, it would be worth more than $1 billion', 'Fire-breathing', 'Half man...', '✨✨✨', 'Check the moon', 'Also a classic Nike running shoes style', 'Can also be found in the comments section', 'Winter is his favorite season'];
+  let guesses, guessesLeft, prevTries, hint, pickNum;
 
   // Game Logic //
 
@@ -33,35 +30,35 @@
     pickNum = Math.floor(Math.random() * answer.length);
     randomAnswer = answer[pickNum].split('');
     hint = hints[pickNum];
-    state = [...randomAnswer].fill(' _ ');
-    displayState = [...randomAnswer].fill(' _ ').join('');
+    state = [...randomAnswer].fill(' _ '); // this displays w/ commas
+    displayState = [...randomAnswer].fill(' _ ').join(''); // this makes it display nicely
     feedbackDiv.textContent = '';
     // console.log(randomAnswer);
     // console.log(state);
   }
 
-  // Remove chosen answer from the array of answers so player doesn't get repeats
+  // Remove chosen answer from the array of answers so player doesn't get repeats during the game, then display a game over message
   function removeAnswer() {
     answer.splice(pickNum, 1);
     if (answer.length === 0) {
-      alert("WOW! You beat the whole game.");
-      // need to add some kind of congratulations message here - as a modal? With a new game buton
+      alert("WOW! You beat the whole game. Nicely done.");
+      // Need to add some kind of congratulations message here as a modal. With play again option
     }
   }
 
-  // Used to ensure guess is one letter that hasn't been tried yet
+  // Used to ensure guess is a unique letter
   function isValidGuess(guess) {
-    return guess && guess.length === 1 && guess.match(/[a-z]/i) && !prevTries.includes(guess);
+    return guess && guess.length === 1 && guess.match(/[a-z]/) && !prevTries.includes(guess);
   }
 
-  // Updates the guesses counter and adds incorrect letter to list of letters guessed
+  // Updates the guesses left counter and adds incorrect letter to list of letters guessed
   changeLives = function() {
     guessesLeft--;
     tries.textContent = guessesLeft;
     prevTriesSpan.textContent = prevTries.join(', ');
   };
 
-  // Check if player has won the round, if so display a winner modal
+  // Check if player has won the round, if so display the win modal
   function checkWin(state) {
     if (state.includes(' _ ')) {
       return false;
@@ -72,7 +69,7 @@
     }
   }
 
-  // Winner modal things
+  // Win modal things
   showModal = function() {
     modal.classList.remove('hidden');
   };
@@ -86,19 +83,19 @@
     startGame();
   };
 
-  // Used to return to the beginning nad start new game
+  // Used to return to the beginning and start new round
   function startGame() {
     chooseAnswer();
     removeAnswer();
     guessesLeft = 5;
     prevTries = [];
-    correctGuesses = [];
+    hintArea.innerHTML = `<button>Hint, please!</button>`;
     playSpace.textContent = displayState;
     tries.textContent = guessesLeft;
     prevTriesSpan.textContent = '';
   }
 
-  // Event Listener aka ~THE GAME~
+  // The Game
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     const guess = form.guess.value;
@@ -111,7 +108,7 @@
         }
         // console.log(state);
         playSpace.textContent = state.join(' ');
-        checkWin(state); // was this the last guess needed for a win?
+        checkWin(state);
       } else if (randomAnswer.indexOf(guess) === -1 && guessesLeft > 1) {
         feedbackDiv.textContent = 'That\'s not quite right. Try a different letter!';
         prevTries.push(guess);
@@ -122,16 +119,15 @@
         changeLives();
       } else if (randomAnswer.indexOf(guess) === -1 && guessesLeft === 1) {
         changeLives();
-        // displays feedback and then automatically restarts the game
+        // Displays feedback and then automatically restarts the game
         feedbackDiv.innerHTML = `<h1>Oh no, you lost!</h1>`;
         setTimeout(function() {
           startGame();
         }, 4000);
       }
     } else {
-      feedbackDiv.textContent = 'Uh oh! Please guess something else. Your guess must be a single letter that you have not previously tried.';
+      feedbackDiv.textContent = 'Uh oh! Please guess something else. Your guess must be a single lowercase letter that you have not previously tried.';
     }
-
     form.guess.value = '';
   });
 
